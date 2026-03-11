bb7d281d3cc57161f1a38c534b4f17f82faea0e6
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-11 11:38:10 +0000

    Add coach.service.ts

---
+++ src/app/core/services/coach.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { CoachResponse, CoachCreateRequest } from '../../models/coach.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CoachService {
+  private base = `${environment.apiUrl}/coaches`;
+
+  constructor(private http: HttpClient) {}
+
+  create(req: CoachCreateRequest): Observable<CoachResponse> {
+    return this.http.post<CoachResponse>(this.base, req);
+  }
+
+  listAll(): Observable<CoachResponse[]> {
+    return this.http.get<CoachResponse[]>(this.base);
+  }
+
+  listActive(): Observable<CoachResponse[]> {
+    return this.http.get<CoachResponse[]>(`${this.base}/active`);
+  }
+
+  getById(id: number): Observable<CoachResponse> {
+    return this.http.get<CoachResponse>(`${this.base}/${id}`);
+  }
+
+  update(id: number, req: CoachCreateRequest): Observable<CoachResponse> {
+    return this.http.put<CoachResponse>(`${this.base}/${id}`, req);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.base}/${id}`);
+  }
+}
