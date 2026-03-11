dfd139907335d252a501ab47c653086bc72f380c
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-11 11:38:09 +0000

    Add app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
+@NgModule({
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
+})
+export class AppModule {}
