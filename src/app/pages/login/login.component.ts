d3001269c146c0f7586ca1fbd0889a96e5d2d1fa
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-11 11:38:22 +0000

    Add login.component.ts

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+  loading = false;
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  submit() {
+    this.error = '';
+    this.loading = true;
+    this.auth.login(this.username, this.password).subscribe({
+      next: () => {
+        this.loading = false;
+        if (this.auth.hasRole('ROLE_ADMIN')) this.router.navigate(['/admin']);
+        else if (this.auth.hasRole('ROLE_MEMBER')) this.router.navigate(['/members/me']);
+        else this.router.navigate(['/']);
+      },
+      error: () => {
+        this.loading = false;
+        this.error = 'Nom d\'utilisateur ou mot de passe incorrect';
+      }
+    });
+  }
+}
