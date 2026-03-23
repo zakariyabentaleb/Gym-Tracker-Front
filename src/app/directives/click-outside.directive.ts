392b35023ef9893bac135311c8f844da83d6c027
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 00:06:40 +0100

    Add click-outside.directive.ts

---
+++ src/app/directives/click-outside.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener, Output, EventEmitter, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[clickOutside]',
+  standalone: true
+})
+export class ClickOutsideDirective {
+  @Output() clickOutside = new EventEmitter<void>();
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.clickOutside.emit();
+    }
+  }
+}
