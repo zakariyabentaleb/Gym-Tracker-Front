0314c14803d8b4710b462279ef5e139bc9cc3da3
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 00:06:50 +0100

    Add environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://localhost:8081/api',
   paypalClientId: 'AYG74FQxizmUtE3P7uYdsbKEUd6AvZ7o31mKd0voOto75HZV2adSrLpJETlc_rFK42SuCxMM_Oamwvy7'
 };
